a8d0ea0b868cad4496df2aebce0a46d80a145d7c
ioan.zicu <ioan.zicu@olx.pl>
2024-09-28 12:24:43 +0200

    clean up functional implementation

--- main.ts
+++ main.ts
@@ -9,70 +9,6 @@ import {
   SummaryOutput,
 } from "./types.ts";
 
-function summaryProcessor(data: Summary): SummaryOutput {
-  return {
-    userId: data.userId,
-    activityType: data.activityType,
-    deviceName: data.deviceName,
-    maxHeartRatePerMinute: data.maxHeartRateInBeatsPerMinute,
-    durationInSeconds: data.durationInSeconds,
-  };
-}
-
-function lapsProcessor(data: Laps): LapsOutput {
-  const laps = [];
-  for (const element of data) {
-    laps.push({
-      startTimeInSeconds: element.startTimeInSeconds,
-      distanceInMeters: element.totalDistanceInMeters,
-      durationInSeconds: element.timerDurationInSeconds,
-    });
-  }
-  return laps;
-}
-
-const HEAR_RATE_SAMPLE_TYPE = "2";
-
-function heartRateSamplesDataProcessor(
-  data: HeartRateSamples
-): HeartRateSamplesOutput {
-  const heartRateSamples = [];
-  for (let i = 0; i < data.length; ) {
-    if (data[i]["sample-type"] === HEAR_RATE_SAMPLE_TYPE) {
-      const sample = [
-        {
-          sampleIndex: i,
-          heartRate: data[i].data,
-        },
-      ];
-      i++;
-      sample.push({
-        sampleIndex: i,
-        heartRate: data[i].data,
-      });
-      i++;
-      heartRateSamples.push(sample);
-      continue;
-    }
-    i++;
-  }
-  return heartRateSamples;
-}
-
-function processSamplesOutput(
-  lapsData: LapsOutput,
-  heartRateSamples: HeartRateSamplesOutput
-): Output {
-  const output = [];
-  for (let i = 0; i < lapsData.length; i++) {
-    output.push({
-      ...lapsData[i],
-      heartRateSamples: heartRateSamples[i],
-    });
-  }
-  return output;
-}
-
 class DataProcessor {
   readonly HEAR_RATE_SAMPLE_TYPE: string = "2";
 
@@ -111,7 +47,7 @@ class DataProcessor {
   loadHeartRateSamplesDataProcessor(data: HeartRateSamples): this {
     const heartRateSamples = [];
     for (let i = 0; i < data.length; i++) {
-      if (data[i]["sample-type"] === HEAR_RATE_SAMPLE_TYPE) {
+      if (data[i]["sample-type"] === this.HEAR_RATE_SAMPLE_TYPE) {
         const sample = [];
         sample.push({
           sampleIndex: i,
@@ -122,7 +58,7 @@ class DataProcessor {
 
         if (
           i < data.length &&
-          data[i]["sample-type"] === HEAR_RATE_SAMPLE_TYPE
+          data[i]["sample-type"] === this.HEAR_RATE_SAMPLE_TYPE
         ) {
           sample.push({
             sampleIndex: i,
@@ -166,10 +102,4 @@ class DataProcessor {
   }
 }
 
-export {
-  summaryProcessor,
-  lapsProcessor,
-  heartRateSamplesDataProcessor,
-  processSamplesOutput,
-  DataProcessor,
-};
+export { DataProcessor };
